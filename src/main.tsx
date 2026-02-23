cbdde210b46bfbdd4003a015042de15ad9c66a52
choisiwon <c000317@naver.com>
2026-02-23 12:25:24 +0900

    second

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,16 @@
+// src/main.tsx
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import './index.css'
 import App from './App.tsx'
-import {QueryClient} from '@tanstack/react-query'
 
-const queryClient = new QueryClient();
+const queryClient = new QueryClient()
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <QueryClientProvider client = {queryClient}>
-        <App />
+    <QueryClientProvider client={queryClient}>
+      <App />
     </QueryClientProvider>
   </StrictMode>,
 )
